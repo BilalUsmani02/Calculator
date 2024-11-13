d37602d31eed757ee157d44b4054cc224096d472
Faham94 <syedfahamalinaqvi1357@gmail.com>
2024-11-13 23:31:21 +0500

    added display code
    i added display code for calculator operations

--- CalculatorApp.c
+++ CalculatorApp.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Display the options
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    // Input numbers
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    
+    return 0;
+}
