31a98570e56ce4fcae054d26df7e6a6668ac76e5
Maaz Khan <maaz.ak211@gmail.com>
2024-11-13 23:42:19 +0500

    Added Calculator functions
    Added the calculator functions of addition subtraction multiplication and division

--- CalculatorApp.c
+++ CalculatorApp.c
@@ -11,6 +11,33 @@ int main() {
     // Input numbers
     printf("Enter two operands: ");
     scanf("%lf %lf", &num1, &num2);
+    
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            // Check for division by zero
+            if (num2 != 0)
+                result = num2 / num2;
+            else {
+                printf("Error! Division by zero.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Error! Operator is not correct\n");
+            return 1;
+    }
+
+    // Display the result
+    printf("%.2lf %c %.2lf = %.2lf\n", num1, operator, num2, result);
 
     
     return 0;
